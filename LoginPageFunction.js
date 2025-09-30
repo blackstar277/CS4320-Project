ded04a314e28c2201e1f97ac3132ff95803c055b
SyntaxSanchez <SyntaxSanchez8@gmail.com>
2025-09-29 22:52:30 -0600

    Gitpage

---
+++ LoginPageFunction.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
+
